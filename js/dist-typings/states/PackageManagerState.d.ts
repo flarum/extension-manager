76cfa48bd237014b6edf414d43de250780b75901
Sami Mazouz <sychocouldy@gmail.com>
2024-01-10 15:56:32 +0100

    chore: rename `package-manager` to `extension-manager`

--- js/dist-typings/states/PackageManagerState.d.ts
+++
@@ -1,6 +0,0 @@
-import QueueState from './QueueState';
-import ControlSectionState from './ControlSectionState';
-export default class PackageManagerState {
-    queue: QueueState;
-    control: ControlSectionState;
-}
